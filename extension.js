1020ae91ef0a1bfc0c71448e3c3965c3b8beafb9
BrunoAlcantara <bruno.fracaro.alcantara@gmail.com>
2024-06-06 15:28:22 -0300

    get all Iamge files and JS files

--- extension.js
+++ extension.js
@@ -17,14 +17,37 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('file-searcher.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('file-searcher.helloWorld', async function () {
 		// The code you place here will be executed every time your command is executed
 
+		const allImagesFilesObj = await vscode.workspace.findFiles('**/*.{png,jpg,jpeg,gif,svg}', '**/node_modules/**');
+		const allImagesFilesNames = []
+		for (const item of allImagesFilesObj) {
+			allImagesFilesNames.push(item.path);
+		}
+
+		const allJsFilesObj = await vscode.workspace.findFiles('**/*.js', '**/node_modules/**');
+		const allJsFilesNames = []
+		for (const item of allJsFilesObj) {
+			allJsFilesNames.push(item.path);
+
+			console.log(item.path);
+		}
+
+		const text = await readFileContent(allJsFilesNames[0]);
+
+		console.log(text);
+
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from File Searcher!');
+		vscode.window.showInformationMessage('Hello World from File Searcher Unused Image Highlighter8!');
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+async function readFileContent(filePath) {
+  const document = await vscode.workspace.openTextDocument(vscode.Uri.file(filePath));
+  return document.getText();
 }
 
 // This method is called when your extension is deactivated
